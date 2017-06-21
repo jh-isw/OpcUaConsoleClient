645e85028baf9c7e4a276179c728f68ae1d8f6b3
Jan <jan.hecht@jh-isw.de>
2017-06-21 23:59:08 +0200

    add boost program_options

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <readline/history.h>
 #include <open62541.h>
 #include <csignal>
+#include <boost/program_options.hpp>
+
+using namespace std;
+namespace po = boost::program_options;
 
 void getEndpoints();
 
@@ -14,9 +18,32 @@ static void stopHandler(int sig) {
     running = false;
 }
 
-int main() {
+int main(int argc, const char** argv) {
     signal(SIGINT, stopHandler);
     signal(SIGTERM, stopHandler);
+
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "produce help message")
+            ("url", po::value<string>()->default_value("opc.tcp://localhost:4840"), "set URL of the server to connect to")
+            ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        cout << desc << "\n";
+        return 1;
+    }
+
+    if (vm.count("url")) {
+        cout << "url is "
+             << vm["url"].as<string>() << ".\n";
+    } else {
+        cout << "url default used.\n";
+    }
 
     UA_Client *client = UA_Client_new(UA_ClientConfig_standard);
 
